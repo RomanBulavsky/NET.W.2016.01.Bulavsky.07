d242fe926cbfb26f1cb173fef6380634877ae86b
RomanBulavsky <romanbulavsky@gmail.com>
2016-11-03 22:52:03 +0300

    Task 1 refactoring

---
+++ Task1.Tests/JaggedArrayTests.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Task1.Tests
+{
+    internal class SumUp : IJagged
+    {
+        public int CompareTo(int[] a, int[] b)
+        {
+            var aSum = a.Sum();
+            var bSum = b.Sum();
+
+            if (a == b) return 0;
+
+            return aSum > bSum ? 1 : -1;
+        }
+    }
+
+    internal class SumDown : IJagged
+    {
+        public int CompareTo(int[] a, int[] b)
+        {
+            var aSum = a.Sum();
+            var bSum = b.Sum();
+
+            if (a == b) return 0;
+
+            return aSum < bSum ? 1 : -1;
+        }
+    }
+
+    internal class MaxUp : IJagged
+    {
+        public int CompareTo(int[] a, int[] b)
+        {
+            var aMax = a.Max();
+            var bMax = b.Max();
+
+            if (a == b) return 0;
+
+            return aMax > bMax ? 1 : -1;
+        }
+    }
+
+    internal class MaxDown : IJagged
+    {
+        public int CompareTo(int[] a, int[] b)
+        {
+            var aMax = a.Max();
+            var bMax = b.Max();
+
+            if (a == b) return 0;
+
+            return aMax < bMax ? 1 : -1;
+        }
+    }
+
+    internal class MinUp : IJagged
+    {
+        public int CompareTo(int[] a, int[] b)
+        {
+            var aMin = a.Min();
+            var bMin = b.Min();
+
+            if (a == b) return 0;
+
+            return aMin < bMin ? 1 : -1;
+        }
+    }
+
+    internal class MinDown : IJagged
+    {
+        public int CompareTo(int[] a, int[] b)
+        {
+            var aMin = a.Min();
+            var bMin = b.Min();
+
+            if (a == b) return 0;
+
+            return aMin > bMin ? 1 : -1;
+        }
+    }
+
+
+    public class JaggedArrayTests
+    {
+        //[TestCase(new int[] { 4, 5 },  new int[] { 1, 2, 3 }, new int[] { 1, 2 }, new int[] { 4, 5 })]
+        [TestCase(new[] {4, 5}, new[] {1, 2, 3}, new[] {1, 2}, new[] {4, 5})]
+        [TestCase(new[] {2, 8}, new[] {1, 4, 3}, new[] {8, 2}, new[] {4, 1})]
+        public static void SortJaggedArray_SumUp_RightArrayInTheFirstIndexOfJaggedArray(int[] result,
+            params int[][] jArray) //Extended method
+        {
+            var sorter = new JaggedArraySorter();
+            var cmp = new SumUp();
+            sorter.SortJaggedArray(jArray, cmp);
+
+            Assert.AreEqual(jArray[0], result);
+        }
+
+        [TestCase(new[] {4, 5}, new[] {1, 2, 3}, new[] {2, 1}, new[] {4, 5})]
+        [TestCase(new[] {2, 8}, new[] {1, 4, 3}, new[] {8, 2}, new[] {4, 1})]
+        public static void SortJaggedArray_SumDown_RightArrayInTheLastIndexOfJaggedArray(int[] result,
+            params int[][] jArray) //Extended method
+        {
+            var sorter = new JaggedArraySorter();
+            var cmp = new SumDown();
+            sorter.SortJaggedArray(jArray, cmp);
+
+            Assert.AreEqual(jArray[2], result);
+        }
+
+        [TestCase(new[] {4, 5}, new[] {1, 2, 3}, new[] {2, 1}, new[] {4, 5})]
+        [TestCase(new[] {2, 8}, new[] {1, 4, 3}, new[] {8, 2}, new[] {4, 1})]
+        public static void SortJaggedArray_MaxUp_RightArrayInTheFirstIndexOfJaggedArray(int[] result,
+            params int[][] jArray) //Extended method
+        {
+            var sorter = new JaggedArraySorter();
+            var cmp = new MaxUp();
+            sorter.SortJaggedArray(jArray, cmp);
+
+            Assert.AreEqual(jArray[0], result);
+        }
+
+        [TestCase(new[] {4, 5}, new[] {1, 2, 3}, new[] {2, 1}, new[] {4, 5})]
+        [TestCase(new[] {2, 8}, new[] {1, 4, 3}, new[] {8, 2}, new[] {4, 1})]
+        public static void SortJaggedArray_MaxDown_RightArrayInTheLastIndexOfJaggedArray(int[] result,
+            params int[][] jArray) //Extended method
+        {
+            var sorter = new JaggedArraySorter();
+            var cmp = new MaxDown();
+            sorter.SortJaggedArray(jArray, cmp);
+
+            Assert.AreEqual(jArray[2], result);
+        }
+
+        [TestCase(new[] {1, 2, 3}, new[] {1, 2, 3}, new[] {2, 1}, new[] {4, 5})]
+        [TestCase(new[] {1, 4}, new[] {1, 4, 3}, new[] {8, 2}, new[] {4, 1})]
+        public static void SortJaggedArray_MinUp_RightArrayInTheFirstIndexOfJaggedArray(int[] result,
+            params int[][] jArray) //Extended method
+        {
+            var sorter = new JaggedArraySorter();
+            var cmp = new MinUp();
+            sorter.SortJaggedArray(jArray, cmp);
+
+            Assert.AreEqual(jArray[0], result);
+        }
+
+        [TestCase(new[] {1, 2, 3}, new[] {1, 2, 3}, new[] {2, 1}, new[] {4, 5})]
+        [TestCase(new[] {1, 4}, new[] {1, 4, 3}, new[] {8, 2}, new[] {4, 1})]
+        public static void SortJaggedArray_MinDown_RightArrayInTheLastIndexOfJaggedArray(int[] result,
+            params int[][] jArray) //Extended method
+        {
+            var sorter = new JaggedArraySorter();
+            var cmp = new MinDown();
+            sorter.SortJaggedArray(jArray, cmp);
+
+            Assert.AreEqual(jArray[2], result);
+        }
+
+
+        [TestCase(null, new[] {1, 2, 3}, new[] {1, 2}, new[] {4, 5})]
+        [TestCase(new[] {1, 4, 3}, new[] {2, 8}, null)]
+        public static void ChangeAndSortTest_BadParams_Exception(params int[][] jArray)
+        {
+            var sorter = new JaggedArraySorter();
+            var cmp = new MinDown();
+
+            Assert.That(() => sorter.SortJaggedArray(jArray, cmp), Throws.TypeOf<ArgumentException>());
+        }
+
+
+        [TestCase(null)]
+        public static void ChangeAndSortTest_NullArray_Exception(int[][] jArray)
+        {
+            var sorter = new JaggedArraySorter();
+            var cmp = new MinDown();
+
+            Assert.That(() => sorter.SortJaggedArray(jArray, cmp), Throws.TypeOf<ArgumentException>());
+        }
+
+        [TestCase(null)]
+        public static void ChangeAndSortTest_CmpNull_Exception(int[][] jArray)
+        {
+            var sorter = new JaggedArraySorter();
+            SumUp cmp = null;
+
+            Assert.That(() => sorter.SortJaggedArray(jArray, cmp), Throws.TypeOf<ArgumentException>());
+        }
+    }
+}
---
+++ Task1/JaggedArraySorter.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Linq;
+
+
+namespace Task1
+{
+    /// <summary>
+    /// Class to sort a jagged array fields by the laws passed by the comparator.
+    /// </summary>
+    public static class JaggedArraySorter
+    {
+        /// <summary>
+        ///     Method that choosing type of logic for sorting.
+        ///  Method that provides sorting, by the law that transmitted by the class implementing the IJagged interface.
+        /// </summary>
+        /// <param name="jArray"> Input Jagged Array. </param>
+        /// <param name="comparer"> Class implementing the IJagged interface and supplied CompareTo method that chooses the way for field sorting.</param>
+        public static void SortJaggedArray(int[][] jArray, IJagged comparer)
+        {
+            if (jArray == null || jArray.Any(inner => inner == null) || comparer == null)//tnx ReSharper
+                throw new ArgumentException();
+            for (var i = 0; i < jArray.Length - 1; i++)
+            {
+                for (var j = 0; j < jArray.Length - 1; j++)
+                {
+                    var cmp = comparer.CompareTo(jArray[j], jArray[j + 1]);
+
+                    if (cmp < 0) SwapFields(ref jArray[j], ref jArray[j + 1]);
+                }
+
+            }
+
+            foreach (var item in jArray)
+            {
+                BubbleSort(item);
+            }
+
+        }
+        /// <summary>
+        ///     Sorts the array.
+        /// </summary>
+        private static void BubbleSort(int[] array)
+        {
+            for (var i = 0; i < array.Length - 1; i++)
+            {
+                var swapped = false;
+                for (var j = 0; j < array.Length - i - 1; j++)
+                    if (array[j].CompareTo(array[j + 1]) > 0)
+                    {
+                        var tmp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = tmp;
+                        swapped = true;
+                    }
+
+                if (!swapped)
+                    break;
+            }
+        }
+
+        /// <summary>
+        ///     A method that swaps two fields of the jagged array.
+        /// </summary>
+        private static void SwapFields(ref int[] firstArray, ref int[] secondArray)
+        {
+            var tmp = firstArray;
+            firstArray = secondArray;
+            secondArray = tmp;
+        }
+
+    }
+
+}
